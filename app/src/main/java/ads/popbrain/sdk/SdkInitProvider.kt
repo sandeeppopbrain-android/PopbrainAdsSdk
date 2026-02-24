c2f5183333c90ace371dc9de4567dd9f24e43746
Sandeep Malik <maliksandeep8054@gmail.com>
2026-02-24 16:37:45 +0530

    Auto init  done

---
+++ app/src/main/java/ads/popbrain/sdk/SdkInitProvider.kt
@@ -0,0 +1,37 @@
+package ads.popbrain.sdk
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class SdkInitProvider : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        context?.let {
+            PopbrainAdsSDK.initialize(it)
+        }
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? = null
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int = 0
+}
